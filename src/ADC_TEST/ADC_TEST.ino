d764624575378843966580183bdfa4029307692a
dlwnstjr2004 <dlwnstjr2004@gmail.com>
2019-08-18 15:48:10 +0900

    20190818

--- src/ADC_TEST/ADC_TEST.ino
+++ src/ADC_TEST/ADC_TEST.ino
@@ -1,7 +1,7 @@
 //#define ANALOG_PIN_0 39 // VN -> ADC
 //#define ANALOG_PIN_0 36 // VP -> ADC
 #define ANALOG_PIN_0 33 // ADC
-int LED_BUILTIN = 2;
+int LED_BUILTin = 2;
 int analog_value = 1;
 /* Arduino ADC PIN TEST
  * used esp32 dev module
@@ -9,14 +9,14 @@ int analog_value = 1;
  */
 void setup() {
   // put your setup code here, to run once:
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTin, OUTPUT);
   Serial.begin(115200);
   Serial.println("ESP32 Analog IN Test");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN,HIGH);
+  digitalWrite(LED_BUILTin,HIGH);
   analog_value = analogRead(ANALOG_PIN_0);
   Serial.println(analog_value);
   delay(500);
