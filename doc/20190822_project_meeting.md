b14195a2ee5a566a3b1a14b9cc6419cc86e80206
dlwnstjr2004 <dlwnstjr2004@gmail.com>
2019-08-23 01:48:18 +0900

    20190823

---
+++ doc/20190822_project_meeting.md
@@ -0,0 +1,16 @@
+# 8/19일 현 진행상황
+- Leonardo Keyboard.h 에 Print ScreenShot버튼이 없으므로 PS를 포기했다.
+- 
+- esp32 ble hid 가 컴퓨터로 직접 연결하는 것은 포기
+  - 수신부를 만들기 위해 HM-10 BLUETOOTH BLE 모듈과 aruduino leonardo을 구매
+  - arduino leonardo 보드를 이용하여 컴퓨터의 마우스를 이동
+  - HM-10 모듈을 BAUD = 115200 , NAME = BOOT4DIM으로 변경
+  - HM-10 모듈과 esp32 보드가 auto connect 를 할 수 있도록 esp32 client의   UUID를 HM-10 모듈의 UUID와 맞춰주면 auto connect가능하도록 바뀜.
+- 자이로 센서를 통한 미세값 조정중
+- Reset 버튼을 제작(송신부, 수신부)
+
+# 8/26일 예정상황
+- Restandard 함수 조정
+- timer 함수 제작
+- 수신부 코딩 완료
+- 하드웨어 설계 마무리
--- src/AirMouse_receive_module/AirMouse_receive_module.ino
+++ src/AirMouse_receive_module/AirMouse_receive_module.ino
@@ -1,40 +1,120 @@
-/*
-  Software serial multple serial test
-
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
-
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
-
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
-
- Not all pins on the Leonardo and Micro support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
-
- created back in the mists of time
- modified 25 May 2012
- by Tom Igoe
- based on Mikal Hart's example
-
- This example code is in the public domain.
-
- */
 #include <SoftwareSerial.h>
 #include "Mouse.h"
-
+#include "Keyboard.h"
 
 SoftwareSerial mySerial(10, 11); // RX, TX
+
+int Keyboard_Mouse_mode = 1;
+//0: Off
+//1: Mouse
+//2: Keyboard
+void Logic_start(String read_data_frist){
+  String inString = read_data_frist;
+  String inString_back = inString;
+  String inString_front;
+  int index_x ;  
+  int index_y ; 
+  do{
+    index_x = inString.indexOf('.');     
+    index_y = inString.length(); 
+    inString_front = inString.substring(0, index_x);  
+    inString_back = inString.substring(index_x+1,index_y); 
+    inString = inString_back;
+    if(inString_front == "TF"){
+      switch(Keyboard_Mouse_mode){
+        case 0:Keyboard_Mouse_mode=1;break;
+        case 1:Keyboard_Mouse_mode=2;break;
+        case 2:Keyboard_Mouse_mode=0;break;
+        default :Keyboard_Mouse_mode = 0; break;
+      }
+//      Serial.println("TF");
+    }
+    else if(Keyboard_Mouse_mode == 1){
+      if(inString_front == "RC" ){
+        Mouse.press(MOUSE_RIGHT);
+        delay(1);
+        Mouse.release(MOUSE_RIGHT);
+//        Serial.println("RC");
+      }
+      else if(inString_front == "LC"){
+        Mouse.press(MOUSE_LEFT);
+        delay(1);
+        Mouse.release(MOUSE_LEFT);
+//        Serial.println("LC");
+      }
+      else if(inString_front == "SU"){
+        Mouse.move(0,0,1);
+//        Serial.println("SU");
+      }
+      else if(inString_front == "SD"){
+        Mouse.move(0,0,-1);
+//        Serial.println("SD");
+      }
+      else if(inString_front == "MT"){
+        int number_x = inString.indexOf('/');
+        int number_y = inString.indexOf('.');
+        int move_x = inString.substring(0, number_x).toInt();
+        int move_y = inString.substring(number_x+1, number_y).toInt();
+        Mouse.move(move_x,move_y);
+        inString_back = inString.substring(number_y+1,inString.indexOf('\n')); 
+        inString = inString_back;
+//        Serial.println("MT");
+      }
+    }
+    else if(Keyboard_Mouse_mode == 2){
+      if(inString_front == "SB" ){
+        Keyboard.write(' ');
+//        Serial.println("SB");
+      }
+      else if(inString_front == "ZI"){
+        Keyboard.press(KEY_LEFT_CTRL);
+        delay(1);
+        Mouse.move(0,0,5);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("ZI");
+      }
+      else if(inString_front == "ZO"){
+        Keyboard.press(KEY_LEFT_CTRL);
+        delay(1);
+        Mouse.move(0,0,-5);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("ZO");
+      }
+      else if(inString_front == "AU"){
+        Keyboard.press(KEY_UP_ARROW);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("AU");
+      }
+      else if(inString_front == "AD"){
+        Keyboard.press(KEY_DOWN_ARROW);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("AD");
+      }
+      else if(inString_front == "AL"){
+        Keyboard.press(KEY_LEFT_ARROW);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("AL");
+      }
+      else if(inString_front == "AR"){
+        Keyboard.press(KEY_RIGHT_ARROW);
+        delay(1);
+        Keyboard.releaseAll();
+//        Serial.println("AR");
+      }
+    }
+  }while(inString.length() >= 3);
+}
 
 void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(115200);
   Mouse.begin();
+  Keyboard.begin();
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
   }
@@ -46,47 +126,10 @@ void setup() {
 }
 
 void loop() { // run over and over
-//  if (mySerial.available()) {
-//    Serial.write(mySerial.read());
-//  }
-  if(Serial.available()){
-    String read_data = Serial.readStringUntil('\n');
-    Serial.println(read_data);
-    if(read_data == "up"){
-      //Mouse.move(x축,y축,스크롤)
-      //x축 >= 0 >> 오른쪽 이동
-      //y축 >= 0 >> 아래쪽 이동
-      //스크롤 >= 0 >> 위 이동
-      Mouse.move(0,0,1);
-    }
-    else if(read_data == "down"){
-      Mouse.move(0,0,-1);
-    }
-/*
- * 클릭 사용방법
-  if (clickState == HIGH) {
-    // if the mouse is not pressed, press it:
-    if (!Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.press(MOUSE_LEFT);
-    }
+  if(mySerial.available()){
+    String read_data = mySerial.readStringUntil('\n');
+    Serial.println("read_data : " + read_data);
+    Logic_start(read_data);
+
   }
-  // else the mouse button is not pressed:
-  else {
-    // if the mouse is pressed, release it:
-    if (Mouse.isPressed(MOUSE_LEFT)) {
-      Mouse.release(MOUSE_LEFT);
-    }
-  }
-*/
-/*
- * 인덱스를 이용하여 나누는법
-    String inString = Serial.readStringUntil('\n');
-    int index_x = inString.indexOf(',');     
-    int index_y = inString.length(); 
-    float x = inString.substring(0, index_x).toFloat();     
-    float y = inString.substring(index_x+1,index_y).toFloat(); 
-    
-*/
-  }
-  delay(10);
 }
